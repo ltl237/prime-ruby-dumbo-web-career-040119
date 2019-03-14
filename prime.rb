b85f837dfa4b28501a3e4f03f2f3bddd79aa612c
Lorenzo Llamas <ltl924@gmail.com>
2019-03-14 01:41:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 
 def prime?(your_num)
   possible_nums = (2..your_num-1)
-
   
   if your_num > 1 
     possible_nums.to_a.all? do |trial|
        puts !(your_num % trial != 0)
+     end
   end
 end
