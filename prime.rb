7451d0862984f359fb534b67a77bc2c1f339d8d9
Lorenzo Llamas <ltl924@gmail.com>
2019-03-14 01:46:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(your_num)
+  #possible_nums = (2..your_num-1)
+  
+  if your_num > 1 
+    (2..your_num-1).to_a.all? do |trial|
+        your_num % trial != 0
+     end
+   else
+     return FALSE
+  end
+end
