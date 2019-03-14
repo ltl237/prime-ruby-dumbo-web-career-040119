963bde1b42e325a20cc21ec02a5e186b4987474f
Lorenzo Llamas <ltl924@gmail.com>
2019-03-14 01:15:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 
 def prime?(your_num)
-  (2..your_num-1).to_a do |trial|
-    your_num % trial != 0 
+  if your_num > 0 
+    (2..your_num-1).to_a do |trial|
+      your_num % trial != 0 
+    end
   end
 end
