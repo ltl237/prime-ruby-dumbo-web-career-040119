826b734d6bfc01e399b2874236b285142846a9b3
Lorenzo Llamas <ltl924@gmail.com>
2019-03-14 01:39:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(your_num)
-  possible_nums = (2..your_num-1).to_a
+  possible_nums = (2..your_num-1).to_a.all?
   possible_nums.each do |trial|
     your_num % trial != 0 
   end
